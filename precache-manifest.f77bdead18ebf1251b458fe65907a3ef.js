0bc004a20d91e00f3c73827d071eaf1da6d818f5
Raj Jariwala <rajjariwala5976@gmail.com>
2019-09-25 17:19:19 +0530

    Updates

---
+++ precache-manifest.f77bdead18ebf1251b458fe65907a3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42444dc1b5c557f196f25dd2209d9381",
+    "url": "/nikita-demo/index.html"
+  },
+  {
+    "revision": "cefdaf0293c5e3767669",
+    "url": "/nikita-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5f14fda3b641be3e27bb",
+    "url": "/nikita-demo/static/js/2.0031db6c.chunk.js"
+  },
+  {
+    "revision": "cefdaf0293c5e3767669",
+    "url": "/nikita-demo/static/js/main.9f5282b8.chunk.js"
+  },
+  {
+    "revision": "25ef310c68fafb565886",
+    "url": "/nikita-demo/static/js/runtime-main.48d1f093.js"
+  }
+]);
